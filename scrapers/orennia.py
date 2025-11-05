973a4a2aa16e31728bf2c73a8da33a408cedd9b7
Yannyny <yann.tremenbert@gmail.com>
2025-11-05 02:02:08 +0100

    feat: checked section name before getting jobs for orennia

--- scrapers/orennia.py
+++ scrapers/orennia.py
@@ -9,19 +9,21 @@ def parse_orennia_job_list_from_html(html_str: str, base_url: str) -> list[dict]
     job_listings: list[dict] = []
 
     for section in soup.find_all('section'):
-        category = section.find('h3')
-        if category:
-            category_name = category.get_text(strip=True) if category else "Ungrouped"
-            jobs = section.find_all('a', href=True)
-            for job in jobs:
-                title = job.get_text(strip=True)
-                location = job.find_next('ul').get_text(separator=' ', strip=True) if job.find_next('ul') else 'Location not specified'
-                href = job.get('href')
-                time.sleep(0.6)
-                job_listings.append({
-                    'category': category_name,
-                    'title': title,
-                    'location': location,
-                    'url': href,
-                })
+        h2 = section.find("h2")
+        if h2 and h2.get_text(strip=True).lower() == "explore opportunities":
+            category = section.find('h3')
+            if category:
+                category_name = category.get_text(strip=True)
+                jobs = section.find_all('a', href=True)
+                for job in jobs:
+                    title = job.get_text(strip=True)
+                    location = job.find_next('ul').get_text(separator=' ', strip=True) if job.find_next('ul') else 'Location not specified'
+                    href = job.get('href')
+                    time.sleep(0.6)
+                    job_listings.append({
+                        'category': category_name,
+                        'title': title,
+                        'location': location,
+                        'url': href,
+                    })
     return job_listings
